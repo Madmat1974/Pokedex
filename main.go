ee01bcb87c2d5a2df9eb48bfbe44930ce0f74fd0
Martin <MartinNiswonger@gmail.com>
2025-08-07 17:42:48 -0500

    ch1 L4

--- main.go
+++ main.go
@@ -1,12 +1,25 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 	"strings"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	//s := []string{}
+	scanner := bufio.NewScanner(os.Stdin)
+	var liner string
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+		liner = scanner.Text()
+		s := cleanInput(liner)
+		fmt.Printf("Your command was: %s\n", s[0])
+
+	}
+
 }
 
 func cleanInput(intext string) []string {
