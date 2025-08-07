65936bee9612994bb6925fc72c534fcf8bd0ec5d
Martin <MartinNiswonger@gmail.com>
2025-08-07 15:35:56 -0500

    ch1L3 Tests

--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
 	fmt.Println("Hello, World!")
+}
+
+func cleanInput(intext string) []string {
+	//s := []string{}
+	lowText := strings.ToLower(intext)
+	trimText := strings.TrimSpace(lowText)
+	s := strings.Fields(trimText)
+	return s
 }
---
+++ repl_test.go
@@ -0,0 +1,46 @@
+package main
+
+import "testing"
+
+func TestCleanInput(t *testing.T) {
+	cases := []struct {
+		input    string
+		expected []string
+	}{
+		{
+			input:    "  hello  world  ",
+			expected: []string{"hello", "world"},
+		},
+
+		{
+			input:    "crappy stuff",
+			expected: []string{"crappy", "stuff"},
+		},
+
+		{
+			input:    "   12334521   poo Mars rover i am",
+			expected: []string{"12334521", "poo", "mars", "rover", "i", "am"},
+		},
+	}
+
+	for _, c := range cases {
+		actual := cleanInput(c.input)
+		// Check the length of the actual slice against the expected slice
+		// if they don't match, use t.Errorf to print an error message
+		// and fail the test
+		if len(actual) != len(c.expected) {
+			t.Errorf("expected slice length %d, got %d", len(c.expected), len(actual))
+		}
+
+		for i := range actual {
+			word := actual[i]
+			expectedWord := c.expected[i]
+			if word != expectedWord {
+				t.Errorf("expected word %q, got %q", expectedWord, word)
+			}
+			// Check each word in the slice
+			// if they don't match, use t.Errorf to print an error message
+			// and fail the test
+		}
+	}
+}
