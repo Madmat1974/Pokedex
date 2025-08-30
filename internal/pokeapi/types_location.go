5345d365db2f94177969e413de4183c4de5b22b5
Martin <MartinNiswonger@gmail.com>
2025-08-30 00:12:43 -0500

    ch2 l2 refactored big time

--- internal/pokeapi/types_location.go
+++ internal/pokeapi/types_location.go
@@ -1,14 +1,12 @@
 package pokeapi
 
-// go
-
-type NamedAPIRef struct {
-	Name string `json:"name"`
-	URL  string `json:"url"`
-}
-
-type LocationAreaList struct {
-	Next     *string       `json:"next"`
-	Previous *string       `json:"previous"`
-	Results  []NamedAPIRef `json:"results"`
+// RespShallowLocations -
+type RespShallowLocations struct {
+	Count    int     `json:"count"`
+	Next     *string `json:"next"`
+	Previous *string `json:"previous"`
+	Results  []struct {
+		Name string `json:"name"`
+		URL  string `json:"url"`
+	} `json:"results"`
 }
